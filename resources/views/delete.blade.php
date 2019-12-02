15b4b4ab28d555a34bc96bc70277e714e1efb681
Dillon Constantine <dillonconstantinedeveloper@gmail.com>
2019-12-02 08:15:42 +0000

    Fix typos

--- resources/views/delete.blade.php
+++ resources/views/delete.blade.php
@@ -7,8 +7,8 @@
         @csrf
         <div class="flex flex-wrap -mx-2">
             <div class="w-full px-2">
-                <label for="delete">
-                    Delete
+                <label for="id">
+                    ID
                 </label>
                 <input id="id" name="id" type="text" placeholder="10" value="{{ old('id') }}" class="placeholder-gray-500 @error('delete') error-form-input @enderror">
                 @error('id')
